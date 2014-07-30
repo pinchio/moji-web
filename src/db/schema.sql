fa6c7bf67711ce4a03f2a7846cea910e8a85c6a6
Charlie Liang Yuan <charlie@betable.com>
2014-07-29 20:02:19 -0700

    add other tables

--- src/db/schema.sql
+++ src/db/schema.sql
@@ -8,6 +8,46 @@ CREATE TABLE account (
     username        varchar(15),
     password        varchar(128),
     created_at      timestamptz,
+    updated_at      timestamptz,
     born_at         timestamptz,
     UNIQUE(username)
 );
+
+CREATE TABLE moji (
+    id              char(36) PRIMARY KEY,
+    created_at      timestamptz,
+    updated_at      timestamptz,
+    display_name    varchar(128),
+    slug_name       varchar(128),
+    tags            varchar(128)[],
+    privacy         varchar(128)[],
+    created_by      char(36),
+    UNIQUE(slug_name)
+);
+
+CREATE TABLE moji_stat (
+    id              char(36) PRIMARY KEY,
+    last_sent_at    timestamptz,
+    sent_count      integer,
+    download_count  integer
+);
+
+CREATE TABLE moji_collection (
+    id              char(36) PRIMARY KEY,
+    created_at      timestamptz,
+    updated_at      timestamptz,
+    display_name    varchar(128),
+    slug_name       varchar(128),
+    tags            varchar(128)[],
+    privacy         varchar(128)[],
+    created_by      char(36),
+    UNIQUE(slug_name)
+);
+
+CREATE TABLE moji__moji_collection (
+    id              char(36) PRIMARY KEY,
+    moji_id         char(36),
+    moji_collection_id char(36),
+    created_at      timestamptz,
+    UNIQUE(moji_id, moji_collection_id)
+)
