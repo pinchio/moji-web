d0c9794174fc03fbe3edb0f3e0c7f5abf3a382e8
Jeff Wang <jeffkwang@gmail.com>
2014-08-28 18:09:29 -0700

    basic splash page

--- src/home/HomePage.js
+++ src/home/HomePage.js
@@ -18,43 +18,25 @@ var HomePage = React.createClass({
         return (
             <div className={this.state.classNames.join(' ')}>
                 <div className="page">
-                    <HomeHeaderComponent
-                        toggle_menu={this.props.toggle_menu}
-                        scroll_body_to_top={this.scroll_page_to_top} />
                     <div className="page-body-container">
                         <div className="page-body">
-                            <div className="sprite-app-store-badge-container">
-                                <div className="sprite sprite-app-store-badge-en"></div>
+                            <div className="sprite-home-icon-bg-container">
+                                <div className="sprite sprite-home-icon-bg"></div>
                             </div>
-                            <p>asdf</p>
-                            <p>asdf</p>
-                            <p>asdf</p>
-                            <p>asdf</p>
-                            <p>asdf</p>
-                            <p>asdf</p>
-                            <p>asdf</p>
-                            <p>asdf</p>
-                            <p>asdf</p>
-                            <p>asdf</p>
-                            <p>asdf</p>
-                            <p>asdf</p>
-                            <p>asdf</p>
-                            <p>asdf</p>
-                            <p>asdf</p>
-                            <p>asdf</p>
-                            <p>asdf</p>
-                            <p>asdf</p>
-                            <p>asdf</p>
-                            <p>asdf</p>
-                            <p>asdf</p>
-                            <p>asdf</p>
-                            <p>asdf</p>
-                            <p>asdf</p>
-                            <p>asdf</p>
-                            <p>asdf</p>
-                            <p>asdf</p>
-                            <p>asdf</p>
+                            <div className="home-cta-container">
+                                <div className="sprite-home-logo-container">
+                                    <div className="sprite sprite-home-logo"></div>
+                                </div>
+                                <p>Create your own stickers</p>
+                                <br/><br/>
+                                <a href="https://itunes.apple.com/us/app/imojiapp/id912008559?mt=8">
+                                    <div className="sprite sprite-app-store-badge-en"></div>
+                                </a>
+                            </div>
                         </div>
+                    </div>
+                    <div className="footer">
+                        © 2014, Mojigram. <a href="mailto:direct@mojigram.com">Contact Us</a>
                     </div>
                 </div>
             </div>
